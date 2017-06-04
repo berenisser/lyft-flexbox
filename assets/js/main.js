3280134da48134259ad3cdefe7896723c8f0bda4
berenisser <berenisser@gmail.com>
2017-06-04 09:44:11 -0400

    Footer in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
 window.addEventListener('scroll', function () {
-  const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+  const distanciaVertical = window.pageYOffset || document.documentElement.scrollTop,
   header = document.getElementById('header');
   
-  if (distanceY > 60) {
+  if (distanciaVertical > 60) {
     header.classList.add("non-opaque");
+    document.getElementById("logo").classList.add("logo-rosa");
+    document.getElementById("uno").classList.add("link-gray");
+    document.getElementById("dos").classList.add("link-gray");
+    document.getElementById("tres").classList.add("link-gray");
+    document.getElementById("cuatro").classList.add("cuatro-borde");
+    document.getElementById("cinco").classList.add("cinco-aparece");
   } else {
     header.classList.remove("non-opaque");
+    document.getElementById("logo").classList.remove("logo-rosa");
+    document.getElementById("uno").classList.remove("link-gray");
+    document.getElementById("dos").classList.remove("link-gray");
+    document.getElementById("tres").classList.remove("link-gray");
+    document.getElementById("cuatro").classList.remove("cuatro-borde");
+    document.getElementById("cinco").classList.remove("cinco-aparece");
   }
 });
