e69992e48a9f737405469c1e68d466d89c4fe933
berenisser <berenisser@gmail.com>
2017-06-05 09:51:05 -0400

    Validaciones para revisar

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,82 @@ var inputfields = Array.from(document.getElementsByClassName("hide"));
 	});
 
 /*-----Validaciones-----*/
+
+document.getElementById("btn-send").addEventListener("click", function(){
+//Llamamos los valores de los inputs
+	event.preventDefault(); //previene que la página salte al inicio
+
+	var inputNombre = document.getElementById("value-nombre");
+	var inputEmail = document.getElementById("value-email");
+	var inputFono = document.getElementById("value-fono");
+	var inputTexto = document.getElementById("value-texto");
+
+	
+
+	if (validarNombre(inputNombre)){
+		if(validarEmail(inputEmail)){
+			if(validarFono(inputFono)){
+				if (validarTexto(inputTexto)){
+				alert("Gracias por ingresar su información de contacto");
+				}
+			}
+		}
+	}
+})
+
+
+var mensajito, mensajitoTexto;
+//Funciones de validacion
+function validarNombre(inputNombre) {   
+	var letrasNombre = /^[A-Za-z]+$/;  
+	if(inputNombre.value.match(letrasNombre)){  
+		return true;  
+	} else {  
+		/*mensajito = document.createElement("div");
+		mensajitoTexto = document.createTextNode("Please enter your name");
+		mensajito.appendChild(mensajitoTexto);
+		inputNombre.appendChild(mensajito); */
+		inputNombre.value = ""; 
+		alert("Please enter your name");
+		inputNombre.focus();  
+		return false;  
+	}  
+} 
+
+
+function validarEmail(inputEmail){  
+	var formatoCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+	if(inputEmail.value.match(formatoCorreo)){  
+		return true;  
+	} else {  
+		inputEmail.value = "";
+		alert("Please enter your email address");  
+		inputEmail.focus();  
+		return false;  
+	}  
+} 
+
+function validarFono(inputNumber){
+	var fono = /^[0-9]{9,11}$/;
+	if(inputNumber.value.match(fono)){
+		inputNumber.value = "";
+		return true;
+	}else{
+		inputNumber.value = "";
+		alert("Please enter your phone number");  
+		inputNumber.focus();  
+		return false; 
+	}
+}
+
+function validarTexto(inputTexto){
+	var valueTexto = inputTexto.value.length;
+	if(valueTexto == 0){
+		inputTexto.value = "";
+		alert("Please enter a message");
+		inputTexto.focus();
+		return false;
+	} else{
+		return true;
+	}
+}
